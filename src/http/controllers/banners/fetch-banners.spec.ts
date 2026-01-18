093ea2b6770186e9eabfe8072b53ce305cbc652d
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2026-01-17 23:16:13 -0300

    feat: create a test database and splited e2e test and unit test and environments

--- src/http/controllers/banners/fetch-banners.spec.ts
+++ src/http/controllers/banners/fetch-banners.spec.ts
@@ -1,14 +1,26 @@
 import { app } from '@/app.js'
+import { prisma } from '@/libs/prisma.js'
 import request from 'supertest'
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 
 describe('Fetch Banners (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
 
+  beforeEach(async () => {
+    await prisma.banner.deleteMany()
+    await prisma.banner.create({
+      data: {
+        image: 'fake_banner.svg',
+        link: '/categories/test',
+      },
+    })
+  })
+
   afterAll(async () => {
     await app.close()
+    await prisma.$disconnect()
   })
 
   it('should be able to fetch banners', async () => {
